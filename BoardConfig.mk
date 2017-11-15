c5c7fb7768545f1b88555ce2a4ae64349a2842c6
DennySPB <dennyspb@gmail.com>
2017-11-15 12:05:08 +0300

    Enable realtime battery info if supported

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -113,6 +113,7 @@ TARGET_SUPPORT_HAL1 := false
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
 
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 

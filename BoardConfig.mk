957ebbf643db2420b9131ecea3ceca50f0afcf43
DennySPB <dennyspb@gmail.com>
2017-11-30 16:00:00 +0300

    Use qcom sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -207,7 +207,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
-#BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # Vendor init
 TARGET_INIT_VENDOR_LIB := libinit_gemini

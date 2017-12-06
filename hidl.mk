c3cf3152b82cf73f2513508e2a42e59b1d8a57da
DennySPB <dennyspb@gmail.com>
2017-12-06 12:42:43 +0300

    Unbind Sensor HAL

--- hidl.mk
+++ hidl.mk
@@ -96,8 +96,7 @@ PRODUCT_PACKAGES += \
 
 # Sensors HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 # USB HIDL
 PRODUCT_PACKAGES += \

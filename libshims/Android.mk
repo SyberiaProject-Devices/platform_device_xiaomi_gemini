174447a9f2e524c6fe08516505ca0ae295e2bf3f
DennySPB <dennyspb@gmail.com>
2017-12-19 12:35:34 +0300

    Revert "msm8996-common: libshims: Update IMS shim"
    This reverts commit e188afa49a5426d5aa9a558cfe8f96237b25c937.

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -16,13 +16,12 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := MediaBuffer.cpp
+LOCAL_SRC_FILES := MediaBuffer.c
 
-LOCAL_SHARED_LIBRARIES := libstagefright_foundation libui libgui
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
 
 LOCAL_MODULE := libshims_ims
 LOCAL_MODULE_TAGS := optional
-LOCAL_MULTILIB := 64
 
 include $(BUILD_SHARED_LIBRARY)
 

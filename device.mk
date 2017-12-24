008f30defbfeb6dc1c4886759eae9642a6465b51
Bruno Martins <bgcngm@gmail.com>
2017-12-24 02:29:51 +0000

    msm8996-common: Move Wi-Fi files to vendor
    Change-Id: I7e95baa01490ec76cf8ddfa051f01fc9b0bc5c7a

--- device.mk
+++ device.mk
@@ -321,11 +321,11 @@ PRODUCT_PACKAGES += \
     Spectrum
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
-    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf
+    $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+    $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # HIDL
 $(call inherit-product, $(LOCAL_PATH)/hidl.mk)

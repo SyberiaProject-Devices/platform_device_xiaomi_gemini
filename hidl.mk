6564b140f865787239b8e774308c63b0ef04023f
LuK1337 <priv.luk@gmail.com>
2018-01-17 19:13:33 +0100

    msm8996-common: Go back to generic vibrator service
    * No one really likes how Lineage vibrator service works,
      we may come back to this sooner or later with our own
      service but for now let's just go back to generic one.
    
    Change-Id: Iafa36f4d00717b86144d88170e9bdae697ef4ff9

--- hidl.mk
+++ hidl.mk
@@ -111,7 +111,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-impl \
+    android.hardware.vibrator@1.0-service
 
 # WiFi HIDL
 PRODUCT_PACKAGES += \

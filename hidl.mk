a092d55ea741ae2e68b2b1fab27c5ddde6c23096
DennySPB <dennyspb@gmail.com>
2018-01-22 14:15:32 +0300

    HILD: Remove generic IR hals

--- hidl.mk
+++ hidl.mk
@@ -72,12 +72,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.0-impl-qti \
     android.hardware.gnss@1.0-service-qti
 
-# IR HIDL
-PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-impl \
-    android.hardware.ir@1.0-service
-
-
 # Keymaster HIDL
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@3.0-impl \

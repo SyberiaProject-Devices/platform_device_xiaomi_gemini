6f769b11ab13891b92b3c84ec31cc948f8b70d9e
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-02 19:39:50 -0800

    msm8996-common: Set shipping API level via inherited makefile
     * PRODUCT_SHIPPING_API_LEVEL is set there
    
    Change-Id: I8adecfbaafb538277f6451a5dbc8abc279bf0c15

--- device.mk
+++ device.mk
@@ -15,6 +15,7 @@
 #
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/xiaomi/gemini/gemini-vendor.mk)

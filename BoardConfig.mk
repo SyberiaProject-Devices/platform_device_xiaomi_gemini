7ea259c8d5d42a8bd11e6b17d1b200a98dea6fec
DennySPB <dennyspb@gmail.com>
2018-03-05 15:36:00 +0300

    BoardConfig: add back Interraction_boost

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -203,6 +203,8 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Power
 TARGET_HAS_NO_WIFI_STATS := true
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
+TARGET_USES_INTERACTION_BOOST := true
+
 
 # QCOM
 BOARD_USES_QCOM_HARDWARE := true

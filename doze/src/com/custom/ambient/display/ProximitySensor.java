c9d95776a14a72d8811b5c6d75cb6bed944ce8dc
ezio84 <brabus84@gmail.com>
2018-06-14 15:00:19 +0200

    msm8996-common: doze: use ExecutorService for listener registration
    Registering a sensor seems to be an expensive operation,
    and we do it on each screen-on event, so moving it to
    an asynchronous task looks like a good idea anyway.
    
    By moving all non-essential binder calls of the main thread or to the
    next frame, we bring this down to 5ms, such that window animation
    and Keyguard animation starts about at the same time.
    https://developer.android.com/reference/java/util/concurrent/ExecutorService
    
    Change-Id: I4f37bb9a7dc9d7775d587d4ebd4b6619f3b77e81

--- doze/src/com/custom/ambient/display/ProximitySensor.java
+++ doze/src/com/custom/ambient/display/ProximitySensor.java
@@ -23,6 +23,10 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
 public class ProximitySensor implements SensorEventListener {
 
     private static final boolean DEBUG = false;
@@ -36,15 +40,20 @@ public class ProximitySensor implements SensorEventListener {
     private SensorManager mSensorManager;
     private Sensor mSensor;
     private Context mContext;
+    private ExecutorService mExecutorService;
 
     private boolean mSawNear = false;
     private long mInPocketTime = 0;
 
     public ProximitySensor(Context context) {
         mContext = context;
-        mSensorManager = (SensorManager)
-                mContext.getSystemService(Context.SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        mSensorManager = mContext.getSystemService(SensorManager.class);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY, false);
+        mExecutorService = Executors.newSingleThreadExecutor();
+    }
+
+    private Future<?> submit(Runnable runnable) {
+        return mExecutorService.submit(runnable);
     }
 
     @Override
@@ -83,12 +92,16 @@ public class ProximitySensor implements SensorEventListener {
 
     protected void enable() {
         if (DEBUG) Log.d(TAG, "Enabling");
-        mSensorManager.registerListener(this, mSensor,
-                SensorManager.SENSOR_DELAY_NORMAL);
+        submit(() -> {
+            mSensorManager.registerListener(this, mSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        });
     }
 
     protected void disable() {
         if (DEBUG) Log.d(TAG, "Disabling");
-        mSensorManager.unregisterListener(this, mSensor);
+        submit(() -> {
+            mSensorManager.unregisterListener(this, mSensor);
+        });
     }
 }

45745460fb8e71ad2af3dc83b16123a162daf7ba
LuK1337 <priv.luk@gmail.com>
2018-06-29 18:44:41 +0200

    msm8996-common: Remove camera.device@3.2-impl from PRODUCT_PACKAGES
    * camera.device@3.2-impl is a dependency of camera provider
      therefore there's no need to add it to PRODUCT_PACKAGES.
    
    Change-Id: I3ae211965a31b5f85af3dd0fa1c7811b802586c3

--- hidl.mk
+++ hidl.mk
@@ -31,8 +31,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service \
-    camera.device@3.2-impl
+    android.hardware.camera.provider@2.4-service
 
 # Display HIDL
 PRODUCT_PACKAGES += \

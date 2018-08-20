63ecff0ff2de5d6877b08606e9acd95be78c2324
DennySPB <dennyspb@gmail.com>
2018-08-20 16:10:38 +0300

    NFC: Switch to 1.1 HAL

--- hidl.mk
+++ hidl.mk
@@ -83,8 +83,7 @@ PRODUCT_PACKAGES += \
 
 # NFC HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.0-impl \
-    android.hardware.nfc@1.0-service
+    android.hardware.nfc@1.1-service
 
 # Power HIDL
 PRODUCT_PACKAGES += \

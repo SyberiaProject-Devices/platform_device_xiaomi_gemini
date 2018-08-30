41f4ce935a8b3555018527a95359b24d8a8afa9b
Bindu Gokul <bgokul@codeaurora.org>
2018-08-30 14:58:18 +0530

    Set partitions for loop device.
    AdoptableHostTest in CTS uses virtual disk feature for testing.
    This change is to enable partitions for virtual disk.
    
    Change-Id: Ie03766738715ef54d4fa768033a96fdd76d3172b
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -54,6 +54,7 @@ DEVICE_MATRIX_FILE := $(VENDOR_PATH)/compatibility_matrix.xml
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom ehci-hcd.park=3 cma=32M@0-0xffffffff
 BOARD_KERNEL_CMDLINE += firmware_class.path=/vendor/firmware_mnt/image
+BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100

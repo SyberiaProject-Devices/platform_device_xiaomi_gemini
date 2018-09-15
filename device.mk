60e76960d6b41348510b1a1fae73e98f48361bb3
Ivan Blinov <blinoff82@users.noreply.github.com>
2018-09-15 13:29:14 +0300

    It's time for telephony-ext

--- device.mk
+++ device.mk
@@ -323,11 +323,11 @@ PRODUCT_COPY_FILES += \
 
 
 # Telephony
-#PRODUCT_PACKAGES += \
-#    telephony-ext
+PRODUCT_PACKAGES += \
+    telephony-ext
 
-#PRODUCT_BOOT_JARS += \
-#    telephony-ext
+PRODUCT_BOOT_JARS += \
+    telephony-ext
 
 # Thermal
 PRODUCT_PACKAGES += \

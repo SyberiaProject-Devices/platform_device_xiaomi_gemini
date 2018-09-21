3ae09d39750ed45ea6340d79a1854d2508ba3c86
ThEMarD <themard85@gmail.com>
2018-09-21 03:24:59 -0600

    msm8996: update config.fs from CAF
    LA.UM.7.5.r1-02200-8x96.0
    
    Change-Id: Ib518f505e37585728632ae768d298c6f724af460

--- config.fs
+++ config.fs
@@ -47,7 +47,13 @@ caps: NET_BIND_SERVICE
 mode: 0755
 user: AID_SYSTEM
 group: AID_RADIO
-caps: NET_BIND_SERVICE
+caps: NET_BIND_SERVICE BLOCK_SUSPEND
+
+[vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
 
 [vendor/bin/imsrcsd]
 mode: 0755
@@ -76,6 +82,18 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 

0ab3c5e993d1663175f495c2b851bb8b5a5769bf
Dante Russo <drusso@codeaurora.org>
2018-09-26 17:07:24 -0700

    Add properties used for network location provider
    ro.location.osnlp.package is set to the OS specific
    network location provider, where the region network
    location provider is ro.location.osnlp.region.package.
    
    Change-Id: If531b4cf0805a4bcf1ce9d9d488fed2c15ffaf49
    CRs-fixed: 2323467
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -222,3 +222,19 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.lmk.upgrade_pressure=100 \
     ro.lmk.downgrade_pressure=100 \
     ro.lmk.kill_heaviest_task=true
+
+# ro.location properties below are used in conjunction
+# with NLP_MODE setting in /vendor/etc/izat.conf
+# either as the main network provider or as a fallback
+# provider depending on NLP_MODE that is set
+# These properties are optional and will not cause
+# issues if not set to existing packages.
+# ro.location.osnlp.package sets the name of default
+# OS sepecific network location provider package to use
+# ro.location.osnlp.region.package is set to the name
+# of the alternative network location provider package
+# to be used for a particular region where the default
+# network location provider is not functional
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.location.osnlp.package=com.google.android.gms \
+    ro.location.osnlp.region.package=

78d319606fd2e371522e106ca18c0e749a20892c
DennySPB <dennyspb@gmail.com>
2018-10-05 10:11:10 +0300

    hidl: build 1.1 DRM clearkey
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -47,7 +47,8 @@ PRODUCT_PACKAGES += \
 # DRM HIDL
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.1-service.clearkey
 
 # Fingerprint HIDL
 PRODUCT_PACKAGES += \

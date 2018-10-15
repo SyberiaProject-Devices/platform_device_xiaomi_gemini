7c117aa908ddf2b10e40b51b9d8acc0c888b27f5
DennySPB <dennyspb@gmail.com>
2018-10-15 09:17:53 +0300

    [SQUASH] Revert all audio changes to 2.0 interface
    Some users have issues with 4.0 interface
    
    Change-Id: If5accfa1bf2b758a60eaf41a6f7c2c4e2994b5fa
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -16,10 +16,12 @@
 
 # Audio HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio@2.0-service \
-    android.hardware.audio.effect@4.0-impl \
-    android.hardware.soundtrigger@2.1-impl
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service
 
 # Camera
 PRODUCT_PACKAGES += \

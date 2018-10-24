38254ef6f133d68985693c7ec86d155b514f8ed3
DennySPB <dennyspb@gmail.com>
2018-10-24 09:06:12 +0300

    Set userspace LMKD properties
    We doesen't use kernel LMK. Set default google's values for now
    
    Change-Id: Ie78e8ba237ba444818ea07ae3bb9f28027976666
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -214,3 +214,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.qti.inputopts.movetouchslop=0.6 \
     ro.qcom.adreno.qgl.ShaderStorageImageExtendedFormats=0 \
     sys.use_fifo_ui=1
+
+#LMKD props
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true

4650bbeff8f4d07058c45506bee85155a4cf1e4b
DennySPB <dennyspb@gmail.com>
2018-11-23 16:11:02 +0300

    Kand WFD blobs from Mi A2
    from Jasmine PKQ1.180904.001 V10.0.1.0.PDIMIFJ
    
    Change-Id: I0e03744f0d2359b5ebf62432f17fdd5dc1228d48
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- device.mk
+++ device.mk
@@ -370,6 +370,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Wfd
+PRODUCT_BOOT_JARS += \
+    WfdCommon
+
 # HIDL
 $(call inherit-product, $(LOCAL_PATH)/hidl.mk)
 
